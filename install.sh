ae74ddb5c21f9a912bfde002c37f8cb7a2f22cea
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2020-05-02 17:40:05 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!
+# coded by cybereagle2001
+git clone https://github.com/Moham3dRiahi/Th3inspector.git
+git clone https://github.com/trustedsec/social-engineer-toolkit.git
+git clone https://github.com/sherlock-project/sherlock.git
+git clone https://github.com/cybereagle2001/Crypto.git
+git clone https://github.com/Mebus/cupp.git
+git clone https://github.com/nmap/ncrack.git
+git clone https://github.com/vanhauser-thc/thc-hydra.git
+chmod +x STIA.sh
