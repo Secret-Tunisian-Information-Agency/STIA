eddaf1e06829eba04096ddc98a7de85012436190
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2020-05-16 14:15:54 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!
 # coded by cybereagle2001
+echo *******************************
+echo         please wait
+echo *******************************
+sudo apt-get update
 git clone https://github.com/Moham3dRiahi/Th3inspector.git
 git clone https://github.com/trustedsec/social-engineer-toolkit.git
 git clone https://github.com/sherlock-project/sherlock.git
@@ -7,4 +11,6 @@ git clone https://github.com/cybereagle2001/Crypto.git
 git clone https://github.com/Mebus/cupp.git
 git clone https://github.com/nmap/ncrack.git
 git clone https://github.com/vanhauser-thc/thc-hydra.git
+sudo apt-get install figlet 
 chmod +x STIA.sh
+echo to start the script use ./STIA.sh
