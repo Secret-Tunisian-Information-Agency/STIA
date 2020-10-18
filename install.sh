ecd74df425ced2766f677ebb37aec5414ec63766
Oussama Ben Hadj Dahman <63789665+cybereagle2001@users.noreply.github.com>
2020-10-18 10:55:17 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,12 +6,11 @@ echo  "       please wait"
 echo "*******************************"
 sudo apt-get update
 git clone https://github.com/Moham3dRiahi/Th3inspector.git
+git clone https://github.com/BullsEye0/shodan-eye.git
 git clone https://github.com/trustedsec/social-engineer-toolkit.git
-git clone https://github.com/sherlock-project/sherlock.git
 git clone https://github.com/cybereagle2001/Crypto.git
 git clone https://github.com/Mebus/cupp.git
 git clone https://github.com/nmap/ncrack.git
 git clone https://github.com/vanhauser-thc/thc-hydra.git
 sudo apt-get install figlet 
-chmod +x STIA.sh
-echo "to start the script use ./STIA.sh"
+python STIATOOLKIT.py
